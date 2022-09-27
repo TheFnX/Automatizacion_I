e640812c77ca34db956ae481e06a03593c9859be
TheFnX <beto.dubal19@gmail.com>
2022-09-26 22:34:19 -0400

    2

--- src/main/java/pages/DispatchOptionsPage.java
+++ src/main/java/pages/DispatchOptionsPage.java
@@ -1,10 +1,27 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DispatchOptionsPage extends BasePage{
+
+    private By departmentSelect = By.id("region");
+    private By countrySelect = By.id("ciudad");
+    private By neighborhoodSelect = By.id("comuna");
+    private By continueButton = By.name("field04");
+
     public DispatchOptionsPage(WebDriver webDriver) {
         super(webDriver);
 
+    }
+
+    public DispatchDataPage dispatchOptionsAs(String department,
+                                              String country,
+                                              String neighborhood){
+        selectByVisibleText(department, departmentSelect);
+        selectByVisibleText(country, countrySelect);
+        selectByVisibleText(neighborhood, neighborhoodSelect);
+        webDriver.findElement(continueButton).click();
+        return new DispatchDataPage(webDriver);
     }
 }
